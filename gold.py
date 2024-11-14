5e6aa4701b6f52fda50cdbfee7b227dea444a9c0
anukansha102 <anukansha102>
2024-11-14 06:52:55 +0000

    fine-tuned views

--- gold.py
+++ gold.py
@@ -4,28 +4,27 @@ spark.sql("CREATE SCHEMA IF NOT EXISTS curated_emobility")
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC CREATE OR REPLACE VIEW curated_emobility.us_charger_count_by_county AS
+# MAGIC %sql 
+# MAGIC CREATE OR REPLACE VIEW curated_emobility.us_charger_count_by_state AS 
 # MAGIC SELECT
-# MAGIC     cl.county AS County,
-# MAGIC     cl.city AS City,
-# MAGIC     SUM(CASE WHEN cc.power_type IN ('AC (Single-Phase)', 'AC (Three-Phase)') THEN 1 ELSE 0 END) AS ac_charger_count,
-# MAGIC     SUM(CASE WHEN cc.power_type = 'DC' THEN 1 ELSE 0 END) AS dc_charger_count,
-# MAGIC     COUNT(*) AS total_charger_count
-# MAGIC FROM
-# MAGIC     euh_emobility.charger_location cl
-# MAGIC JOIN
-# MAGIC     euh_emobility.charger_connector cc ON cl.location_id = cc.evse_id
-# MAGIC WHERE
-# MAGIC     cl.country_code = 'US'
-# MAGIC     AND cc.power_type IS NOT NULL
-# MAGIC GROUP BY
-# MAGIC     cl.county, cl.city;
+# MAGIC cl.county AS county,
+# MAGIC cl.city as city, 
+# MAGIC COUNT(*) AS charger_count, 
+# MAGIC     CASE 
+# MAGIC         WHEN cc.power_type = 'AC (Single-Phase)' OR cc.power_type = 'AC (Three-Phase)'THEN 'AC' 
+# MAGIC         WHEN cc.power_type = 'DC' THEN 'DC' 
+# MAGIC         ELSE 'Unknown' 
+# MAGIC     END 
+# MAGIC AS charger_type 
+# MAGIC FROM euh_emobility.charger_location cl 
+# MAGIC JOIN euh_emobility.charger_connector cc ON cl.location_id = cc.evse_id 
+# MAGIC WHERE cl.country_code = 'US' 
+# MAGIC GROUP BY cl.county, cl.city, charger_type;
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC SELECT * FROM curated_emobility.us_charger_count_by_county;
+# MAGIC SELECT * FROM curated_emobility.us_charger_count_by_state;
 
 # COMMAND ----------
 
@@ -52,6 +51,38 @@ spark.sql("CREATE SCHEMA IF NOT EXISTS curated_emobility")
 
 # MAGIC %sql
 # MAGIC SELECT * FROM curated_emobility.us_charger_count_by_power;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE OR REPLACE VIEW curated_emobility.us_charger_count_by_power_details AS
+# MAGIC SELECT
+# MAGIC     CASE
+# MAGIC         WHEN cc.power_kw <= 7 THEN 'Slow (<= 7 kW)'
+# MAGIC         WHEN cc.power_kw <= 22 THEN 'Fast (> 7 kW and <= 22 kW)'
+# MAGIC         WHEN cc.power_kw <= 50 THEN 'Rapid (> 22 kW and <= 50 kW)'
+# MAGIC         ELSE 'Ultra-Rapid (> 50 kW)'
+# MAGIC     END AS power_category,
+# MAGIC     COUNT(*) AS charger_count,
+# MAGIC     cl.county,
+# MAGIC     cl.operator
+# MAGIC FROM
+# MAGIC     euh_emobility.charger_connector cc
+# MAGIC JOIN
+# MAGIC     euh_emobility.charger_location cl ON cc.evse_id = cl.location_id
+# MAGIC JOIN
+# MAGIC     euh_emobility.charger_evse ce ON cc.evse_id = ce.evse_id
+# MAGIC WHERE
+# MAGIC     cl.country_code = 'US'
+# MAGIC GROUP BY
+# MAGIC     power_category,
+# MAGIC     cl.county,
+# MAGIC     cl.operator
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM curated_emobility.us_charger_count_by_power_details;
 
 # COMMAND ----------
 
